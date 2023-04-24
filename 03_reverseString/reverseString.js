7a155620084f92f6280dc433bb0b463269b7be63
Keyhanrezvan <Keyhanrezvan@gmail.com>
2023-04-23 23:05:28 -0500

    reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+
+    let opp = string.split("") //takes string a breaks down into array of letters
+    let rev = opp.reverse()  //takes array and reverses
+  let reversed= rev.join("")  //takes array and joins creating a string
+
+return reversed
 };
 
+console.log(reverseString("duck"))
 // Do not edit below this line
 module.exports = reverseString;
