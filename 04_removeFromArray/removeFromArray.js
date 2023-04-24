45b5ba76528a13584f5223d34f860c3b12cce934
Keyhanrezvan <Keyhanrezvan@gmail.com>
2023-04-23 23:53:22 -0500

    removeitemfromarray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...vari) {
+    let ex= []
+    array.forEach(elm=>{
+ if (!vari.includes(elm)){   // if the element is not in the list of items to be excluded (...vari) then push to ex array
+    ex.push(elm)
+ }
+    })
 
+    return ex
 };
 
+console.log(removeFromArray([1,2,3],2))
 // Do not edit below this line
 module.exports = removeFromArray;
